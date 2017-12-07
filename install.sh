b5d33596a4c75e31ab05be6a65ce7b6223d4dce7
Adi M <adim.moldovan@gmail.com>
2017-12-07 16:38:37 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -3,18 +3,10 @@
 serverpath=/var/lib/tomcat8/webapps
 basepath=$(dirname $(readlink -f $0))
 basedir=$(basename $basepath)
-finalpath=$serverpath/$basedir
 
 echo "Packaging war file"
 mvn clean package
-
 cd target
-
-echo -e "\nUsing path $finalpath"
-echo "Removing $finalpath"
-rm -rf $finalpath
-mkdir $finalpath
-
-echo "Copying war file to $finalpath"
+echo -e "\nUsing path $serverpath"
+echo "Copying war file to $serverpath"
 rsync -zarv --include="*.war" --exclude="*" "." "$serverpath"
-cd $finalpath
