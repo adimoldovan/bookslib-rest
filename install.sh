58ad892d3eca9a36f7c63c61a821ae26f2057a03
Adi M <adim.moldovan@gmail.com>
2017-12-07 17:33:33 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ serverpath=/var/lib/tomcat8/webapps
 basepath=$(dirname $(readlink -f $0))
 basedir=$(basename $basepath)
 
-echo "Packaging war file"
+echo ">> Packaging war file"
 mvn clean package
 cd target
-echo -e "\nUsing path $serverpath"
-echo "Copying war file to $serverpath"
+echo -e "\n>> Using path $serverpath"
+echo -e ">> Stopping tomcat8"
+service tomcat8 restart
+echo ">> Copying war file to $serverpath"
 rsync -zarv --include="*.war" --exclude="*" "." "$serverpath"
 chown tomcat8:tomcat8 $serverpath/*.war
-service tomcat8 restart
+echo -e ">> Starting tomcat8"
+service tomcat8 start
