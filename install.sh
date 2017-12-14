a3f9726a6a2acba1a52f6e0a9b890d8615c9a959
Adi M <adim.moldovan@gmail.com>
2017-12-14 11:43:02 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ echo -e ">> Stopping tomcat8"
 service tomcat8 stop
 echo ">> Copying war file to $serverpath"
 rsync -zarv --include="*.war" --exclude="*" "." "$serverpath"
+chown tomcat8:tomcat8 $serverpath
 chown tomcat8:tomcat8 $serverpath/*.war
 echo -e ">> Starting tomcat8"
 service tomcat8 start
