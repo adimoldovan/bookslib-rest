97ffb749590d1015844aae6a322a760e320fbfd5
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2020-02-20 15:46:15 +0200

    Cleanup

--- src/main/java/am/tau/bookslib/db/MySQLClient.java
+++ src/main/java/am/tau/bookslib/db/MySQLClient.java
@@ -9,7 +9,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MySQLClient {
-    public static final String DB_CONN_STRING = "jdbc:mysql://bookslib-db:3306/bookslib?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final Logger LOGGER = LoggerFactory.getLogger(MySQLClient.class.getName());
     private Connection conn;
     private PreparedStatement preparedStatement;
@@ -46,14 +45,12 @@ public class MySQLClient {
     }
 
     private void connect() throws SQLException {
-        String DB_USER = "bookslibraryrest";
-        String DB_PASSWORD = "bookslibraryrest";
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        conn = DriverManager.getConnection(DB_CONN_STRING, DB_USER, DB_PASSWORD);
+        conn = DriverManager.getConnection(System.getProperty("DB_URL"), System.getProperty("DB_USERNAME"), System.getProperty("DB_PASSWORD"));
     }
 
     public Connection getConnection() throws SQLException {
